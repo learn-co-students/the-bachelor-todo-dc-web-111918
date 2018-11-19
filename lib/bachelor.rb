74ba0465d501ec2c1551885e83e1b162acb963f9
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-19 01:20:46 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,53 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |info|
+    info.each do |person, name|
+      if info["name"] == name
+          return info["name"].split(" ").first
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season,info|
+    info.each do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, info|
+    info.each do |contestant|
+      if contestant["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  return count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, info|
+    info.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  contestant_age = 0
+  count = 0
+  data[season].each do |info|
+      contestant_age += (info["age"]).to_i
+      count += 1
+    end
+  average_age = (contestant_age/count.to_f).round(0)
+  return average_age
 end
